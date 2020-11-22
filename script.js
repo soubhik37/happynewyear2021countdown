2dc13e1ea7dc01094abc0e2cc2d88c37b065a0a8
Soubhik Mondal <46368314+soubhik37@users.noreply.github.com>
2020-11-23 00:01:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const newYears = '1 Jan 2021';
+
+function countdown() {
+    const newYearDate = new Date(newYears);
+    const currentDate = new Date();
+    const gapSeconds = (newYearDate- currentDate)/1000;
+    const days = Math.floor(gapSeconds /86400);
+    const hours = Math.floor(gapSeconds/3600)%24;
+    const mins = Math.floor(gapSeconds/60)%1440;
+    const seconds = Math.floor(gapSeconds)%60;
+    console.log(days +" "+ hours +" "+ mins +" "+ seconds);
+    document.getElementById('days').innerHTML=days;
+    document.getElementById('hours').innerHTML=hours;
+    document.getElementById('mins').innerHTML=mins;
+    document.getElementById('seconds').innerHTML=seconds;
+}
+countdown();
+setInterval(countdown,999);
